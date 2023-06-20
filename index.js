5e434f916e08e9cc250ed6b4227f0d311a9fb713
Paul Cap <118994869+paulcap510@users.noreply.github.com>
2023-06-20 16:29:35 +0900

    First commit.
    First commit.

---
+++ index.js
@@ -0,0 +1,38 @@
+homeScore = 0
+awayScore = 0
+let scoreBoxHomeEl = document.getElementById('scorebox-home')
+let scoreBoxAwayEl = document.getElementById('scorebox-away')
+
+
+function homeAdd1() {
+    homeScore += 1
+    console.log(homeScore)
+    scoreBoxHomeEl.textContent = homeScore
+}
+
+function homeAdd2() {
+    homeScore += 2
+    console.log(homeScore)
+    scoreBoxHomeEl.textContent = homeScore
+}
+
+function homeAdd3() {
+    homeScore += 3
+    console.log(homeScore)
+    scoreBoxHomeEl.textContent = homeScore
+}
+
+function awayAdd1() {
+    awayScore +=1
+    scoreBoxAwayEl.textContent = awayScore
+}
+
+function awayAdd2() {
+    awayScore += 2
+    scoreBoxAwayEl.textContent = awayScore
+}
+
+function awayAdd3() {
+    awayScore += 3
+    scoreBoxAwayEl.textContent = awayScore
+}
